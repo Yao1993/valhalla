640206ef97091591f77c31d61fe92c20a85bfe10
yaoxu <yaoxu@mail.ustc.edu.cn>
2018-01-30 20:35:14 +0800

    Add inner_product and sort targets

--- bench/config.py
+++ bench/config.py
@@ -3,8 +3,7 @@ import collections
 BenchConfig = collections.namedtuple('BenchConfig', ['hdf_filename', 'targets', 'options', 'bin_dir', 'config'])
 
 bench_config = BenchConfig(hdf_filename='E5_2686V3_GTX_1080.hdf',
-                           targets=['reduce', 'saxpy', 'inner_product'],
-                        #    targets=['inner_product'],
+                           targets=['reduce', 'saxpy', 'inner_product', 'sort'],
                            options=['f', '1000', '100000000', 'x10'],
                            bin_dir='../bin',
                            config='Release')
