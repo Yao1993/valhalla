3aa2b5b148e6e3ee4c5bc0fb0d7727218807fc6d
yaoxu <yaoxu@mail.ustc.edu.cn>
2018-03-05 15:48:39 +0800

    Visualize time

--- bench/analyze.py
+++ bench/analyze.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os, errno
 
 from config import bench_config
 
@@ -31,8 +32,15 @@ def calc_bandwidth(data):
     data['bandwidth'] = pd.Series({index: f(row) for index, row in data.iterrows()})
     return data
 
+def mk_dir(dirname):
+    try:
+        os.makedirs(dirname)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
-def visualize(target, data):
+
+def visualize_bandwidth(target, data, output_dir):
     plt.figure(dpi=300)
     for _, row in data.iterrows():
         plt.loglog(row['sequence'], row['bandwidth'],
@@ -42,11 +50,24 @@ def visualize(target, data):
     plt.ylabel('Bandwidth (GBPS)')
     plt.title('{} performance'.format(target))
     plt.legend()
-    plt.savefig("{}.eps".format(target))
+    plt.savefig("{}/{}_bandwidth.eps".format(output_dir, target))
 
+def visualize_time(target, data, output_dir):
+    plt.figure(dpi=300)
+    for _, row in data.iterrows():
+        plt.loglog(row['sequence'], row['time'],
+                   '.-',
+                   label=get_label(row['library'], row['system'], row['backend']))
+    plt.xlabel('Vector Size (Number of elements)')
+    plt.ylabel('Time (s)')
+    plt.title('{} performance'.format(target))
+    plt.legend()
+    plt.savefig("{}/{}_time.eps".format(output_dir,target))
 
 if __name__ == '__main__':
     for target in bench_config.targets:
         bench_df = load_hdf(bench_config.hdf_filename, target)
         bench_df = calc_bandwidth(bench_df)
-        visualize(target, bench_df)
+        mk_dir('./figures')
+        visualize_bandwidth(target, bench_df, './figures')
+        visualize_time(target, bench_df, './figures')
